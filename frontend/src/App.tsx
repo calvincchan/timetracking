9597fbb50c91e87a146872614992fc2b00821398
Calvin Chan <hello@calvin-c.com>
2026-05-29 15:46:14 -0700

    chore(frontend): populate package-lock.json with timetracker dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,54 @@
+import { Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import { liveProvider } from "@refinedev/supabase";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import { Toaster } from "./components/refine-ui/notification/toaster";
+import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import { accessControlProvider } from "./providers/access-control-provider";
+import authProvider from "./providers/auth";
+import { dataProvider } from "./providers/data";
+import { supabaseClient } from "./providers/supabase-client";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <ThemeProvider>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider(supabaseClient)}
+              authProvider={authProvider}
+              accessControlProvider={accessControlProvider}
+              routerProvider={routerProvider}
+              notificationProvider={useNotificationProvider()}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <Toaster />
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ThemeProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
